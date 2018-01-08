c0a19a6413903057bdd7de06d42ed89246dba92a
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-08 18:41:57 +0800

    first commit

---
+++ src/components/Records.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Record from './Record';
+
+class Records extends Component {
+  constructor() {
+    super();
+    this.state = {
+      records: [
+        {"id": 1, "date": "2018-01-09", "title": "收入", "amount": 20},
+        {"id": 2, "date": "2018-01-03", "title": "录视频收入", "amount": 199},
+        {"id": 2, "date": "2018-01-03", "title": "录视频收入", "amount": 199},
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Records</h2>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.records.map((record) => <Record record={record} />)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Records;
