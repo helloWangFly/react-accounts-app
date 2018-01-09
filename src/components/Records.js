35606f492b23a6e7d3f8c01520feb1f762863033
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-09 19:46:08 +0800

    axios

--- src/components/Records.js
+++ src/components/Records.js
@@ -1,36 +1,57 @@
 import React, { Component } from 'react';
 import Record from './Record';
+import axios from 'axios';
 
 class Records extends Component {
   constructor() {
     super();
     this.state = {
-      records: [
-        {"id": 1, "date": "2018-01-09", "title": "收入", "amount": 20},
-        {"id": 2, "date": "2018-01-03", "title": "录视频收入", "amount": 199},
-        {"id": 3, "date": "2018-01-03", "title": "录视频收入", "amount": 199},
-      ]
+      error: null,
+      isLoaded: false,
+      records: []
     }
   }
 
+  componentDidMount() {
+    axios.get("https://5a54227777e1d20012fa0723.mockapi.io/api/v1/records").then(
+      response => this.setState({
+        records: response.data,
+        isLoaded: true
+      })
+    ).catch(
+      error => this.setState({
+        isLoaded: true,
+        error
+      })
+    )
+  }
+
   render() {
-    return (
-      <div>
-        <h2>Records</h2>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Title</th>
-              <th>Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.records.map((record) => <Record key={record.id} record={record} />)}
-          </tbody>
-        </table>
-      </div>
-    );
+    const { error, isLoaded, records } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <h2>Records</h2>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Title</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record) => <Record key={record.id} {...record} />)}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
   }
 }
 
